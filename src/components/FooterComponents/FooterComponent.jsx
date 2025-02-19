c021f9cfac4f160b573b4ed92bdd330050748b7b
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-02-19 16:15:48 +0100

    Refactor FooterComponent layout and styles; add background image

--- src/components/FooterComponents/FooterComponent.jsx
+++ src/components/FooterComponents/FooterComponent.jsx
@@ -1,55 +1,14 @@
 import React from "react";
-
+import style from "./FooterComponent.module.css";
 function FooterComponent() {
   return (
-    <>
-      <hr />
-      <div className="d-flex justify-content-around">
-        <ul className="list-unstyled">
-          <span className="fw-bold">Assistenza</span>
-          <li>Centro assistenza </li>
-          <li>Antidiscriminazione</li>
-          <li>Supporto alla disabilità </li>
-          <li>opzioni di cancellazione</li>
-        </ul>
-        <ul className="list-unstyled">
-          <span className="fw-bold">Ospitare</span>
-          <li>Risorse per gli host</li>
-          <li>Forum della community</li>
-          <li>Ospitare responsabilmente</li>
-        </ul>
-        <ul className="list-unstyled">
-          <span className="fw-bold">AirBnB</span>
-          <li>Newsroom</li>
-          <li>Nuove funzionalità</li>
-          <li>Soggiorni con BoolBnb</li>
-        </ul>
+    <div className={style.footerContainer}>
+      <div className="d-flex justify-content-center align-items-center">
+        <p className="text-white fw-bold">
+          © 2023 BoolBnB, Inc. All rights reserved.
+        </p>
       </div>
-      <hr />
-      <div className="d-flex justify-content-between">
-        <ul className="list-unstyled d-flex gap-3">
-          <li>© 2025 Boolbnb, Inc</li>
-          <li>privacy</li>
-          <li>termini</li>
-          <li>mappa del sito</li>
-          <li>dettagli azienda</li>
-        </ul>
-        <ul className="list-unstyled d-flex gap-3 align-items-center">
-          <li>italiano IT </li>
-          <li>€EUR</li>
-          <li>
-            {" "}
-            <i className="fa-brands fa-facebook"></i>
-          </li>
-          <li>
-            <i className="fa-brands fa-twitter"></i>
-          </li>
-          <li>
-            <i className="fa-brands fa-instagram"></i>
-          </li>
-        </ul>
-      </div>
-    </>
+    </div>
   );
 }
 
