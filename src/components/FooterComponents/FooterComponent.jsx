0961869d68a07bc4daf22e05aed450af241621fc
giuse <giuseppedefilippo03@gmail.com>
2025-02-19 01:44:04 +0100

    aggiunto resposnive al footer

--- src/components/FooterComponents/FooterComponent.jsx
+++ src/components/FooterComponents/FooterComponent.jsx
@@ -3,53 +3,66 @@ import React from "react";
 function FooterComponent() {
   return (
     <>
-      <hr />
-      <div className="d-flex justify-content-around ">
-        <ul className="list-unstyled">
-          <span className="fw-bold">Assistenza</span>
-          <li>Centro assistenza </li>
-          <li>Antidiscriminazione</li>
-          <li>Supporto alla disabilità </li>
-          <li>opzioni di cancellazione</li>
-        </ul>
-        <ul className="list-unstyled">
-          <span className="fw-bold">Ospitare</span>
-          <li>Risorse per gli host</li>
-          <li>Forum della community</li>
-          <li>Ospitare responsabilmente</li>
-        </ul>
-        <ul className="list-unstyled">
-          <span className="fw-bold">BoolBnB</span>
-          <li>Newsroom</li>
-          <li>Nuove funzionalità</li>
-          <li>Soggiorni con BoolBnb</li>
-        </ul>
-      </div>
-      <hr />
-      <div className="d-flex justify-content-between ms-5 me-5 "  >
-        <ul className="list-unstyled d-flex gap-3">
-          <li>© 2025 Boolbnb, Inc</li>
-          <li>privacy</li>
-          <li>termini</li>
-          <li>mappa del sito</li>
-          <li>dettagli azienda</li>
-        </ul>
-        <ul className="list-unstyled d-flex gap-3 align-items-center">
-          <li>italiano IT </li>
-          <li>€EUR</li>
-          <li>
-            {" "}
-            <i className="fa-brands fa-facebook"></i>
-          </li>
-          <li>
-            <i className="fa-brands fa-twitter"></i>
-          </li>
-          <li>
-            <i className="fa-brands fa-instagram"></i>
-          </li>
-        </ul>
-      </div>
+      <div className="container-fluid">
+        <hr />
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-4 text-center mb-3">
+            <ul className="list-unstyled">
+              <span className="fw-bold">Assistenza</span>
+              <li>Centro assistenza</li>
+              <li>Antidiscriminazione</li>
+              <li>Supporto alla disabilità</li>
+              <li>Opzioni di cancellazione</li>
+            </ul>
+          </div>
 
+          <div className="col-12 col-md-4 text-center mb-3">
+            <ul className="list-unstyled">
+              <span className="fw-bold">Ospitare</span>
+              <li>Risorse per gli host</li>
+              <li>Forum della community</li>
+              <li>Ospitare responsabilmente</li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-4 text-center mb-3">
+            <ul className="list-unstyled">
+              <span className="fw-bold">BoolBnB</span>
+              <li>Newsroom</li>
+              <li>Nuove funzionalità</li>
+              <li>Soggiorni con BoolBnb</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="row justify-content-between align-items-center ms-5 me-5">
+          <div className="col-12 col-md-6 text-center text-md-start">
+            <ul className="list-unstyled d-flex gap-3 justify-content-center justify-content-md-start mb-0">
+              <li>© 2025 Boolbnb, Inc</li>
+              <li>Privacy</li>
+              <li>Termini</li>
+              <li>Mappa del sito</li>
+              <li>Dettagli azienda</li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-6 text-center text-md-end">
+            <ul className="list-unstyled d-flex gap-3 justify-content-center justify-content-md-end align-items-center mb-0">
+              <li>Italiano IT</li>
+              <li>€EUR</li>
+              <li>
+                <i className="fa-brands fa-facebook"></i>
+              </li>
+              <li>
+                <i className="fa-brands fa-twitter"></i>
+              </li>
+              <li>
+                <i className="fa-brands fa-instagram"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
