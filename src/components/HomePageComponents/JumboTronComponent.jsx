dcc83d0c8c1ffbdaa7621b22743887e2b232f062
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-02-19 15:40:42 +0100

    Update JumboTronComponent styles and structure for improved background and layout

--- src/components/HomePageComponents/JumboTronComponent.jsx
+++ src/components/HomePageComponents/JumboTronComponent.jsx
@@ -2,9 +2,9 @@ import style from "./JumboTronComponent.module.css";
 
 export default function JumboTronComponent() {
   return (
-    <div className={style.jumbotron}>
+    <div className={`py-3 ${style.jumbotron}`}>
       <div className="container d-flex flex-column justify-content-center align-items-center">
-        <h1 className="fw-bold text-center text-white py-5 font">BoolBnB</h1>
+        <h1 className="fw-bold text-center text-white py-5 ">BoolBnB</h1>
 
         <p className="text-white w-75">
           Viaggiare significa scoprire, esplorare e sentirsi a casa ovunque. Con
