8307a12a9c70c0b54c67c39f02ee6e3505329e65
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-02-19 12:41:02 +0100

    Implement property listing with like functionality and loading state

--- src/components/HomePageComponents/HomePageComponent.jsx
+++ src/components/HomePageComponents/HomePageComponent.jsx
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./HomePageStyle.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { NavLink } from "react-router-dom";
+import JumboTronComponent from "./JumboTronComponent";
 
 function HomePageComponent() {
-  return <div>HomePageComponent</div>;
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/properties")
+      .then((response) => {
+        console.log(response.data);
+        setProperties(response.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+        return <div>Errore nel caricare i dati</div>;
+      });
+  }, []);
+
+  const handleLike = (propertyID) => {
+    console.log(propertyID);
+    const property = properties.find((p) => p.id_properties === propertyID);
+
+    if (!property) {
+      console.error("Proprietà non trovata");
+      return;
+    }
+    const newLikes = property.likes + 1;
+    console.log(newLikes);
+    setProperties((prevProperties) =>
+      prevProperties.map((property) =>
+        property.id === propertyID ? { ...property, likes: newLikes } : property
+      )
+    );
+
+    axios
+      .patch(`http://localhost:3000/api/properties/${propertyID}/likes`, {
+        likes: newLikes,
+      })
+      .then(() => {
+        /* Con questo then andiamo a rifechare tutti i dati dal backend come se lo stessimo facendo per la prima volta, per far si che si addino i like messi in maniera dinamica */
+        axios
+          .get(`http://localhost:3000/api/properties`)
+          .then((response) => {
+            setProperties(response.data.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  if (loading) return <p>Stiamo Caricando i dati</p>;
+
+  return (
+    <>
+      <JumboTronComponent />
+
+      <div className="container-fluid">
+        <div className="d-flex flex-wrap justify-content-center m-3 gap-3 gy-6">
+          {properties.map((property) => (
+            <div
+              key={property.id_properties}
+              className="card-css" /*style={{width: '15rem'}}*/
+            >
+              <NavLink to={`/properties/${property.id_properties}`}>
+                <img
+                  src={`http://localhost:3000${property.first_image}`}
+                  className=""
+                />
+              </NavLink>
+              <div className="description p-3">
+                <h5>{property.title}</h5>
+                <div className="d-flex justify-content-between">
+                  <p>{property.host_name}</p>
+                  <button
+                    className="heart-but"
+                    onClick={() => handleLike(property.id_properties)}
+                  >
+                    <i className="bi bi-heart-fill"></i>
+                    {property.likes}
+                  </button>
+                </div>
+                <div className="d-flex gap-2">
+                  <p>{property.adress_city},</p>
+                  <p>{property.adress_road}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePageComponent;
---
+++ src/components/HomePageComponents/JumboTronComponent.jsx
@@ -0,0 +1,19 @@
+import style from "./JumboTronComponent.module.css";
+
+export default function JumboTronComponent() {
+  return (
+    <div className={style.jumbotron}>
+      <div className="container d-flex flex-column justify-content-center align-items-center">
+        <h1 className="fw-bold text-center text-white py-5 font">BoolBnB</h1>
+
+        <p className="text-white w-75">
+          Viaggiare significa scoprire, esplorare e sentirsi a casa ovunque. Con
+          BoolBnB puoi trovare soggiorni incredibili in ogni angolo del mondo,
+          dalle metropoli vibranti ai rifugi più nascosti. Che tu cerchi
+          avventura o relax, siamo qui per rendere il tuo viaggio
+          indimenticabile
+        </p>
+      </div>
+    </div>
+  );
+}
