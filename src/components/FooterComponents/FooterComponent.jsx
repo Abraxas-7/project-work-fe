be88604d8d4c406f311111253eeb02a2f11b9b4c
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-02-19 18:29:27 +0100

    Enhance FooterComponent with new layout, social media icons, and updated styles

--- src/components/FooterComponents/FooterComponent.jsx
+++ src/components/FooterComponents/FooterComponent.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import style from "./FooterComponent.module.css";
+import { FaFacebookF, FaXTwitter, FaInstagram } from "react-icons/fa6";
 function FooterComponent() {
   return (
     <div className={style.footerContainer}>
       <div className="d-flex flex-column justify-content-center align-items-center p-5">
-        <h3>BoolBnB</h3>
-        <p></p>
-        <ul>
-          <li>
-            <a href="#"></a>
-          </li>
-        </ul>
+        <h2 className={style.footerTitle}>BoolBnB</h2>
+        <div className={style.footerText}>
+          BoolBnB - Il tuo viaggio inizia qui, la tua pagina invece finisce qui
+        </div>
+        <div className="d-flex ustify-content-center align-items-center gap-3">
+          <div className={style.iconList}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className={style.icon} />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF className={style.icon} />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaXTwitter className={style.icon} />
+            </a>
+          </div>
+        </div>
+        <div className="py-3">© 2025 BoolBnB. Tutti i diritti riservati</div>
       </div>
     </div>
   );
