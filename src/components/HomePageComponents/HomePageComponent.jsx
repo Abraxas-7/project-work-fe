65560bf8e3d183aed18cec2df95951d5e2483f3b
RaffaelePellegrino <waffleaffumicato@gmail.com>
2025-02-21 02:55:36 +0100

    aggiornata l'estetica delle card nella homepage

--- src/components/HomePageComponents/HomePageComponent.jsx
+++ src/components/HomePageComponents/HomePageComponent.jsx
@@ -43,22 +43,23 @@ function HomePageComponent() {
   )
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid ">
       <div className="d-flex flex-wrap justify-content-center m-3 gap-3 gy-6">
         {properties.map((property) => (
           <div key={property.id_properties} className="card-css" /*style={{width: '15rem'}}*/>
             <NavLink to={`/properties/${property.id_properties}`}>
               <img src={property.first_image} className="" />
             </NavLink>
-            <div className="description p-3">
-              <h5>{property.title}</h5>
-              <div className="d-flex justify-content-between">
-                <p>{property.host_name}</p>
-                <button className="heart-but" onClick={() => handleLike(property.id_properties)}><i className="bi bi-heart-fill"></i>{property.likes}</button>
-              </div>
-              <div className="d-flex gap-2">
-                <p>{property.adress_city},</p>
-                <p>{property.adress_road}</p>
+            <div className="description">
+              <p><strong>{property.title}</strong></p>
+              <p>Host:{property.host_name}</p>
+              <div className="d-flex justify-content-between align-items-center">
+                <div>
+                  <p>{property.adress_city}, {property.adress_road} </p>
+                </div>
+                <div>
+                  <button className="heart-but" onClick={() => handleLike(property.id_properties)}><i className="bi bi-heart-fill"></i>{property.likes}</button>
+                </div>
               </div>
             </div>
           </div>
